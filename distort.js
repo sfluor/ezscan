e89f29c1d900e330b0831a3928d27989666030f5
Sami Tabet <salph.tabet@gmail.com>
2020-01-01 18:29:09 +0100

    feat: add bilinear interpolation for better results

--- distort.js
+++ distort.js
@@ -60,13 +60,51 @@ export function distort(img, dst, src_corners) {
     [x_s, y_s] = project_2d(multiply(M, augment_2d_point([x, y])));
 
     // RGBA channels
-    for (let c = 0; c < 4; c++) {
-      // TODO: bilinear interpolation
-      dst.data[i + c] = simpleInterpolation(img, x_s, y_s, c);
-    }
+    // const channels = simpleInterpolation(img, x_s, y_s);
+    const channels = bilinearInterpolation(img, x_s, y_s);
+    channels.forEach((value, nb) => (dst.data[i + nb] = value));
   }
 }
 
-function simpleInterpolation(img, x, y, c) {
-  return img.data[c + 4 * (Math.round(y) * img.width + Math.round(x))];
+function pixIdx(x, y, width) {
+  return 4 * (y * width + x);
+}
+
+function bilinearInterpolation(img, x, y) {
+  // See: https://en.wikipedia.org/wiki/Bilinear_interpolation
+
+  const [x1, y1] = [Math.floor(x), Math.floor(y)];
+  // Add +1 instead of using ceil to avoid having x1 == x2
+  const [x2, y2] = [x1 + 1, y1 + 1];
+
+  const q11 = pixIdx(x1, y1, img.width);
+  const q12 = pixIdx(x1, y2, img.width);
+  const q21 = pixIdx(x2, y1, img.width);
+  const q22 = pixIdx(x2, y2, img.width);
+
+  const res = [];
+
+  // No need to normalize since we always have x2 - x1 == 1 == y2 - y1
+  const f11 = (x2 - x) * (y2 - y);
+  const f12 = (x2 - x) * (y - y1);
+  const f21 = (x - x1) * (y2 - y);
+  const f22 = (x - x1) * (y - y1);
+
+  for (let c = 0; c < 4; c++) {
+    res.push(
+      f11 * img.data[q11 + c] +
+        f12 * img.data[q12 + c] +
+        f21 * img.data[q21 + c] +
+        f22 * img.data[q22 + c]
+    );
+  }
+
+  return res;
+}
+
+function simpleInterpolation(img, x, y) {
+  const start = pixIdx(Math.round(x), Math.round(y));
+  const end = start + 4;
+
+  return img.data.slice(start, end);
 }
