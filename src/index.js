fe59b310d4652c474d722aa389e22077cff6eba2
Sami Tabet <sami.tabet@datadoghq.com>
2022-04-30 15:48:40 +0200

    start moving to react

--- src/index.js
+++ src/index.js
@@ -1,75 +1,17 @@
-import { newDynamicCanvas } from "./dynamic_canvas.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function emptyNode(node) {
-  while (node.firstChild) {
-    node.removeChild(node.firstChild);
-  }
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function showCameraInput(node) {
-  const camLabel = document.createElement("label");
-  camLabel.htmlFor = "camera";
-  camLabel.id = "camera-label";
-
-  const camIcon = document.createElement("img");
-  camIcon.className = "main-icon";
-  camIcon.src = "icons/camera.svg";
-
-  camLabel.appendChild(camIcon);
-  camLabel.appendChild(document.createTextNode("Capture Image"));
-
-  node.appendChild(camLabel);
-}
-
-function displayLoader() {
-  document.getElementById("loader").style.display = "flex";
-}
-
-function hideLoader() {
-  document.getElementById("loader").style.display = "none";
-}
-
-const container = document.getElementById("container");
-showCameraInput(container);
-const cropBtn = document.getElementById("crop-btn");
-const backBtn = document.getElementById("back-btn");
-const camera = document.getElementById("camera");
-
-const reader = new FileReader();
-const distortWorker = new Worker("worker_distort.js");
-
-camera.addEventListener("change", () => {
-  const file = camera.files[0];
-  reader.readAsDataURL(file);
-});
-
-backBtn.addEventListener("click", () => {
-  emptyNode(container);
-  reader.canvas = null;
-  showCameraInput(container);
-});
-
-reader.addEventListener("load", () => {
-  emptyNode(container);
-
-  reader.canvas = newDynamicCanvas();
-  container.appendChild(reader.canvas);
-  reader.canvas.draw(reader.result);
-});
-
-distortWorker.onmessage = function(e) {
-  reader.canvas.putImage(e.data);
-  hideLoader();
-};
-
-cropBtn.addEventListener("click", () => {
-  const { data, corners } = reader.canvas.exportImageData();
-
-  const dst_img = reader.canvas.createImage();
-  displayLoader();
-  distortWorker.postMessage({
-    img: data,
-    dst: dst_img,
-    src_corners: corners
-  });
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
