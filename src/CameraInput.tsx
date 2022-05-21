122bdd47697ed479a055d85430e752f76b53eee5
Sami Tabet <sami.tabet@datadoghq.com>
2022-05-21 22:51:52 +0200

    feat: Add a way to re-crop an image / undo a change

--- src/CameraInput.tsx
+++ src/CameraInput.tsx
@@ -3,24 +3,25 @@ import { ReactComponent as Crop } from '@material-design-icons/svg/round/crop.sv
 import { ReactComponent as RotateLeft } from '@material-design-icons/svg/round/rotate_left.svg';
 import { ReactComponent as RotateRight } from '@material-design-icons/svg/round/rotate_right.svg';
 import { ReactComponent as Camera } from '@material-design-icons/svg/round/camera.svg';
+import { ReactComponent as ArrowBack } from '@material-design-icons/svg/round/arrow_back.svg';
+import { ReactComponent as ArrowForward } from '@material-design-icons/svg/round/arrow_forward.svg';
 import FullScreenDiv from './FullScreenDiv';
 import InteractiveCanvas from './InteractiveCanvas';
 import {
   ImagePair,
   distortImage,
   imageToImageData,
+  imageDataToImage,
   openImageInNewTab,
   rotateImage,
   Direction,
 } from './lib/imgkit';
 import { Quadrilateral } from './lib/geometry';
+import colors from './colors';
 
 // For icons: https://marella.me/material-design-icons/demo/svg/
 
 // TODO: move colors to file
-const primaryColor = '#232946';
-const secondaryColor = 'white';
-
 const buttonStyle: React.CSSProperties = {
   alignItems: 'center',
   background: 'none',
@@ -41,7 +42,7 @@ const buttonStyle: React.CSSProperties = {
 
 function LandingDocumentation() {
   return (
-    <div style={{ color: primaryColor }}>
+    <div style={{ color: colors.primary }}>
       <h2>Welcome on ezscan</h2>
       <div>
         To start scanning stuff hit the <b>Load</b> button below !
@@ -65,7 +66,7 @@ function FileInput({
         onChange={onChange}
         style={{ display: 'none' }}
       />
-      <Camera color={secondaryColor} />
+      <Camera color={colors.secondary} />
       <span>Load</span>
     </label>
   );
@@ -98,7 +99,7 @@ function Button({
 }
 
 function CameraInput() {
-  const [image, setImage] = useState<ImagePair | null>(null);
+  const [images, setImages] = useState<Array<ImagePair>>([]);
   const [corners, setCorners] = useState<Quadrilateral | null>(null);
   const reader = new FileReader();
 
@@ -107,25 +108,47 @@ function CameraInput() {
     if (reader.result) {
       const loadedImage = new Image();
       loadedImage.addEventListener('load', () => {
-        setImage({
-          element: loadedImage,
-          data: imageToImageData(loadedImage),
-        });
+        setImages([
+          {
+            element: loadedImage,
+            data: imageToImageData(loadedImage),
+          },
+        ]);
       });
 
       loadedImage.src = reader.result as string;
     }
   };
 
+  const imageIsLoaded = images.length > 0;
+  const currentImage = imageIsLoaded ? images[images.length - 1] : null;
+
   const onCrop = () => {
     // TODO: use a web worker
-    openImageInNewTab(
-      distortImage((image as ImagePair).data, corners as Quadrilateral)
+    const distorted = distortImage(
+      (currentImage as ImagePair).data,
+      corners as Quadrilateral
     );
+    const htmlImage = new Image();
+    htmlImage.addEventListener('load', () => {
+      setImages([...images, { element: htmlImage, data: distorted }]);
+    });
+
+    htmlImage.src = imageDataToImage(distorted);
+  };
+
+  const onPrevious = () => {
+    setImages(images.slice(0, -1) || []);
+  };
+
+  const onNext = () => {
+    openImageInNewTab((currentImage as ImagePair).data);
   };
 
   const onRotate = (direction: Direction) => {
-    rotateImage(image as ImagePair, direction, setImage);
+    rotateImage(currentImage as ImagePair, direction, (rotated) =>
+      setImages([...images, rotated])
+    );
   };
 
   return (
@@ -137,7 +160,7 @@ function CameraInput() {
           height: '90%',
         }}
       >
-        {image ? (
+        {imageIsLoaded ? (
           <InteractiveCanvas
             sizePct={{ width: 100, height: 85 }}
             onCornersChange={setCorners}
@@ -145,7 +168,7 @@ function CameraInput() {
               animationName: 'fadeIn',
               animationDuration: '500ms',
             }}
-            image={image}
+            image={currentImage as ImagePair}
           />
         ) : (
           <LandingDocumentation />
@@ -158,7 +181,7 @@ function CameraInput() {
           flexDirection: 'row',
           justifyContent: 'space-around',
           width: '100%',
-          backgroundColor: primaryColor,
+          backgroundColor: colors.primary,
           height: '10%',
           alignItems: 'center',
         }}
@@ -170,19 +193,29 @@ function CameraInput() {
             }
           }}
         />
-        {image && (
+        {imageIsLoaded && (
+          <Button name="Previous" action={onPrevious}>
+            <ArrowBack color={colors.secondary} />
+          </Button>
+        )}
+        {imageIsLoaded && (
           <Button name="Rotate left" action={() => onRotate(Direction.Left)}>
-            <RotateLeft color={secondaryColor} />
+            <RotateLeft color={colors.secondary} />
           </Button>
         )}
-        {image && (
+        {imageIsLoaded && (
           <Button name="Rotate right" action={() => onRotate(Direction.Right)}>
-            <RotateRight color={secondaryColor} />
+            <RotateRight color={colors.secondary} />
           </Button>
         )}
-        {image && (
+        {imageIsLoaded && (
           <Button name="Crop" action={onCrop}>
-            <Crop color={secondaryColor} />
+            <Crop color={colors.secondary} />
+          </Button>
+        )}
+        {imageIsLoaded && (
+          <Button name="Next" action={onNext}>
+            <ArrowForward color={colors.secondary} />
           </Button>
         )}
       </footer>
