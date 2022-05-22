92ea6ad83c036918b91afab737e820a8a8f37501
Sami Tabet <sami.tabet@datadoghq.com>
2022-05-22 12:22:56 +0200

    fix types in test

--- src/lib/arrayhelpers.test.ts
+++ src/lib/arrayhelpers.test.ts
@@ -1,6 +1,6 @@
 import reorder from './arrayhelpers';
 
-const reorderTests: [Array<T>, number, number, Array<T>][] = [
+const reorderTests: [Array<number>, number, number, Array<number>][] = [
   [[1, 2, 3], 0, 1, [2, 1, 3]],
   [[1, 2, 3], 2, 0, [3, 1, 2]],
   [[1, 2, 3, 4, 5], 3, 4, [1, 2, 3, 5, 4]],
