9badbbc8366bc25c919f2a8c1b7d23b137c0b368
Sami Tabet <sami.tabet@datadoghq.com>
2022-05-22 14:46:38 +0200

    ui: change palette

--- src/colors.ts
+++ src/colors.ts
@@ -1,9 +1,9 @@
-// https://coolors.co/palette/2b2d42-8d99ae-edf2f4-ef233c-d90429
+// https://colorhunt.co/palette/222831393e4600adb5eeeeee
 export default {
-  primary: '#2B2D42',
-  lightPrimary: '#8D99AE',
-  secondary: '#D90429',
-  lightSecondary: '#EF233C',
-  tertiary: '#EDF2F4',
+  primary: '#222831',
+  lightPrimary: '#393E46',
+  secondary: '#00ADB5',
+  lightSecondary: '#00ADB5',
+  tertiary: '#EEEEEE',
   lightTertiary: 'white',
 };
