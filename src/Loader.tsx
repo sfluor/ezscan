aeeafb9603f2eda5fe03f9b64da1ffb981d32e07
Sami Tabet <sami.tabet@datadoghq.com>
2022-05-26 20:44:45 +0200

    add missing loader file + redirect file

---
+++ src/Loader.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import colors from './colors';
+
+// Credits to https://github.com/SamHerbert/SVG-Loaders
+function Loader() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <svg
+        style={{
+          width: '15%',
+          height: '15%',
+        }}
+        width="105"
+        height="105"
+        viewBox="0 0 105 105"
+        xmlns="http://www.w3.org/2000/svg"
+        fill={colors.tertiary}
+      >
+        <circle cx="12.5" cy="12.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="12.5" cy="52.5" r="12.5" fillOpacity=".5">
+          <animate
+            attributeName="fill-opacity"
+            begin="100ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="52.5" cy="12.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="300ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="52.5" cy="52.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="600ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="92.5" cy="12.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="800ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="92.5" cy="52.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="400ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="12.5" cy="92.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="700ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="52.5" cy="92.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="500ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+        <circle cx="92.5" cy="92.5" r="12.5">
+          <animate
+            attributeName="fill-opacity"
+            begin="200ms"
+            dur="1s"
+            values="1;.2;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          />
+        </circle>
+      </svg>
+    </div>
+  );
+}
+
+export default Loader;
